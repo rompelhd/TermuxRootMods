be6acf3497b101e7601a0cd8296357daa9dda35a
Rompelhd <75935831+rompelhd@users.noreply.github.com>
2024-05-26 21:11:16 +0200

    Create customize.sh

---
+++ customize.sh
@@ -0,0 +1,21 @@
+#!/system/bin/sh
+
+[ -d /system/xbin ] && BIN=/system/xbin || BIN=/system/bin
+if [ -d /sdcard ]; then
+  SDCARD=/sdcard
+elif [ -d /storage/emulated/0 ]; then
+  SDCARD=/storage/emulated/0
+fi
+ui_print "   Setting $SDCARD location."
+
+sed -i "s|<SDCARD>|$SDCARD|g" $MODPATH/system/etc/mkshrc
+
+touch $SDCARD/.customrc
+if [ ! -f $SDCARD/.aliases ]; then
+  ui_print "   Copying .aliases to $SDCARD"
+  cp $MODPATH/custom/.aliases $SDCARD
+else
+  ui_print "   $SDCARD/.aliases found! Backing up and overwriting!"
+  cp -rf $SDCARD/.aliases $SDCARD/.aliases.bak
+  cp -rf $MODPATH/custom/.aliases $SDCARD
+fi
