3cc3ec85903e30859ac1053e23befc590ac6ed28
Rompelhd <75935831+rompelhd@users.noreply.github.com>
2025-03-03 03:49:48 +0100

    Update customize.sh
    Improvements in the customize.sh:
    
    Reduction of code repetition.
    
    Robust error handling.
    
    Simplification of conditional operations.
    
    Code restructuring for better readability and maintainability.

--- customize.sh
+++ customize.sh
@@ -1,83 +1,46 @@
 #!/system/bin/sh
 
-[ -d /system/xbin ] && BIN=/system/xbin || BIN=/system/bin
-if [ -d /sdcard ]; then
-  SDCARD=/sdcard
-elif [ -d /storage/emulated/0 ]; then
-  SDCARD=/storage/emulated/0
+BIN=$( [ -d /system/xbin ] && echo "/system/xbin" || echo "/system/bin" )
+SDCARD=$( [ -d /sdcard ] && echo "/sdcard" || echo "/storage/emulated/0" )
+ROOT_HOME="/data/data/com.termux/files/root-home"
+
+ui_print "   Setting SDCARD location: $SDCARD"
+sed -i "s|<SDCARD>|$SDCARD|g" "$MODPATH/system/etc/mkshrc"
+
+touch "$SDCARD/.customrc"
+
+if [ -f "$SDCARD/.aliases" ]; then
+    ui_print "   $SDCARD/.aliases found! Backing up and overwriting!"
+    mv "$SDCARD/.aliases" "$SDCARD/.aliases.bak"
 fi
-ui_print "   Setting $SDCARD location."
+cp "$MODPATH/custom/.aliases" "$SDCARD"
 
-sed -i "s|<SDCARD>|$SDCARD|g" $MODPATH/system/etc/mkshrc
-
-touch $SDCARD/.customrc
-if [ ! -f $SDCARD/.aliases ]; then
-  ui_print "   Copying .aliases to $SDCARD"
-  cp $MODPATH/custom/.aliases $SDCARD
-else
-  ui_print "   $SDCARD/.aliases found! Backing up and overwriting!"
-  cp -rf $SDCARD/.aliases $SDCARD/.aliases.bak
-  cp -rf $MODPATH/custom/.aliases $SDCARD
-fi
-
-if [ ! -d /data/data/com.termux/files/root-home ]; then
-  mkdir /data/data/com.termux/files/root-home
-  ui_print "   Created root home folder"
-  touch /data/data/com.termux/files/root-home/.bash_history
-  ui_print "   Created .bash_history in root home folder"
-else
-  ui_print "   Root home folder exists"
+if [ ! -d "$ROOT_HOME" ]; then
+    mkdir -p "$ROOT_HOME"
+    touch "$ROOT_HOME/.bash_history"
+    ui_print "   Created root home folder and .bash_history"
 fi
 
 ARCH=$(uname -m)
-ui_print "   Detected architecture: $ARCH"
-
 case $ARCH in
-  aarch64)
-    ARCH_DIR="arm64"
-    ;;
-  armv7l | armv8l)
-    ARCH_DIR="armhf"
-    ;;
-  x86_64)
-    ARCH_DIR="x86_64"
-    ;;
-  *)
-    ui_print "   Unsupported architecture: $ARCH"
-    exit 1
-    ;;
+    aarch64) ARCH_DIR="arm64" ;;
+    armv7l | armv8l) ARCH_DIR="armhf" ;;
+    x86_64) ARCH_DIR="x86_64" ;;
+    *) ui_print "   Unsupported architecture: $ARCH"; exit 1 ;;
 esac
 
-if [ -f $MODPATH/system/etc/arch/$ARCH_DIR/servistatus ]; then
-  cp $MODPATH/system/etc/arch/$ARCH_DIR/servistatus /data/data/com.termux/files/usr/bin
-  chmod +x /data/data/com.termux/files/usr/bin/servistatus
-  ui_print "   Servistatus copied and set executable permission on /data/data/com.termux/files/usr/bin"
-else
-  ui_print "   Servistatus not found in $MODPATH/$ARCH_DIR/"
-fi
+copy_file_if_exists() {
+    local src="$MODPATH/system/etc/arch/$ARCH_DIR/$1"
+    local dest="/data/data/com.termux/files/usr/bin/$1"
+    if [ -f "$src" ]; then
+        cp "$src" "$dest"
+        chmod +x "$dest"
+        ui_print "   $1 copied and set executable permission on $dest"
+    else
+        ui_print "   $1 not found in $MODPATH/$ARCH_DIR/"
+    fi
+}
 
-if [ -f $MODPATH/system/etc/arch/$ARCH_DIR/temps ]; then
-  cp $MODPATH/system/etc/arch/$ARCH_DIR/temps /data/data/com.termux/files/usr/bin
-  chmod +x /data/data/com.termux/files/usr/bin/temps
-  ui_print "   Temps copied and set executable permission on /data/data/com.termux/files/usr/bin"
-else
-  ui_print "   Temps not found in $MODPATH/$ARCH_DIR/"
-fi
-
-if [ -f $MODPATH/system/etc/arch/$ARCH_DIR/cleaner  ]; then
-  cp $MODPATH/system/etc/arch/$ARCH_DIR/cleaner /data/data/com.termux/files/usr/bin
-  chmod +x /data/data/com.termux/files/usr/bin/cleaner
-  ui_print "   Cleaner copied and set executable permission on /data/data/com.termux/files/usr/bin"
-else
-  ui_print "   Cleaner not found in $MODPATH/$ARCH_DIR/"
-fi
-
-if [ -f $MODPATH/system/etc/arch/$ARCH_DIR/fsu  ]; then
-  cp $MODPATH/system/etc/arch/$ARCH_DIR/fsu /data/data/com.termux/files/usr/bin
-  chmod +x /data/data/com.termux/files/usr/bin/fsu
-  ui_print "   Fsu copied and set executable permission on /data/data/com.termux/files/usr/bin"
-else
-  ui_print "   Fsu not found in $MODPATH/$ARCH_DIR/"
-fi
-
-fsu
+for file in servistatus temps cleaner fsu; do
+    copy_file_if_exists "$file"
+done
